d0e8fbfd894e47666a5a3176891e5961883193cd
Valera Tarasevich <valera.tarasevit97@gmail.com>
2018-06-21 18:24:31 +0300

    Second

--- Task10/src/by/epam/java/controller/Main.java
+++ Task10/src/by/epam/java/controller/Main.java
@@ -12,13 +12,16 @@ public class Main {
         System.out.println("Sum digits of number: " + rec.sumNumber(number1));
 
         double number2 = 3;
-        int degree = 1;
+        int degree = 2;
         System.out.println("Pow: " + number2 + " ^ " + degree + " = " + rec.powNumber(number2, degree));
 
         int first = 22;
-        int second = 5;
+        int second = 4;
         System.out.println("Equals number: " + first + " <> " + second + "\t" + rec.equalsNumbersTwo(first, second));
         System.out.println("Equals number: " + first + " <> " + second + "\t" + rec.equalsNumbers(first, second));
+
+        int numberFib = 4;
+        System.out.println("Fibonachi: " + rec.fibonachi(numberFib));
 
     }
 }
--- Task10/src/by/epam/java/logic/Recursion.java
+++ Task10/src/by/epam/java/logic/Recursion.java
@@ -4,6 +4,10 @@ public class Recursion {
 
     private static final int DIVIDER = 10;
     private static final int MAX_SINGLE_DIGIT = 10;
+    private static final int LIMIT_NUMBER_2 = 2;
+    private static final int STEP_BY_FIRST = 1;
+    private static final int STEP_BY_SECOND = 2;
+
 
     public int sumNumber(int number) {
         int tmp;
@@ -11,8 +15,9 @@ public class Recursion {
             return -1;
         }
 
-        if (number < MAX_SINGLE_DIGIT)
+        if (number < MAX_SINGLE_DIGIT) {
             return number;
+        }
 
         tmp = number % DIVIDER;
         return sumNumber(number / DIVIDER) + tmp;
@@ -24,22 +29,20 @@ public class Recursion {
             return -1;
         }
 
-        if (degree == 0)
+        if (degree == 0) {
             return 1;
+        }
 
         return powNumber(number, degree - 1) * number;
     }
 
     public boolean equalsNumbers(int first, int second){
 
-        if (first < 0 || second < 0){
+        if (first <= 0 || second < 0){
             return false;
         }
         else if (first < MAX_SINGLE_DIGIT && second < MAX_SINGLE_DIGIT){
             return first == second;
-        }
-        else if (first == 0) {
-            return false;
         }
 
         int tmp = first % DIVIDER;
@@ -47,9 +50,18 @@ public class Recursion {
     }
 
     public boolean equalsNumbersTwo(int first, int second) {
-
         return first >= 0 && second >= 0 && sumNumber(first) == second;
+    }
 
+    public int fibonachi(int number){
+        if (number <= 0){
+            return -1;
+        }
+        else if (number <= LIMIT_NUMBER_2){
+            return 1;
+        }
+
+        return fibonachi(number - STEP_BY_FIRST) + fibonachi(number - STEP_BY_SECOND);
     }
 
 }
